887c786ac7286fab35dbd25dc58720f08a639e34
Nina M <nina.maerte@hotmail.de>
2023-03-09 14:17:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let arrayOfImages = [
+  "https://cdn.pixabay.com/photo/2016/10/21/14/46/fox-1758183_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/23/13/15/red-panda-1852789_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/05/07/16/14/owl-5142121_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2018/01/09/11/04/dog-3071334_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2018/02/21/05/17/cat-3169476_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2015/02/25/17/56/cat-649164_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2019/05/08/21/21/cat-4189697_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/12/25/09/46/dog-5858985_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2022/07/18/19/57/dog-7330712_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2021/11/27/16/25/animal-6828453_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2014/09/10/17/44/rat-440987_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2017/11/01/22/10/nutria-2909569_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2015/10/01/20/28/animal-967657_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2017/01/20/20/02/seal-1995811_960_720.jpg",
+];
+let youSure = [
+  "https://cdn.pixabay.com/photo/2016/03/03/22/15/seal-1235138_960_720.jpg",
+];
+// array + dropbox → Thema aussuchen ob tiere oder grusel ,...
+//anzahl der Spieler + punkte → "turn is over...its player x turn"
+//array rng bilder + rng posi
+//aus dem hoover noch ein klick machen, wenn man ne karte anklickt, dass die sich dreht
